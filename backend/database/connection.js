fe61883e9f9d7db3aa1d1835be1eed0a95889155
Joao Vitor <jvitorgotardo0514@gmail.com>
2026-05-03 17:19:16 -0300

    Corrige erros de conexão ao banco2

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,29 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'postgres',
-        dialectOptions: {
-            connectTimeout: 60000,
-            ssl: {
-                require: true,
-                rejectUnauthorized: false 
-            }
-        },
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        },
-        logging: false
-    }
-);
+// Ele vai tentar usar a string completa do Render, se não tiver, usa a local
+const sequelize = new Sequelize(process.env.DATABASE_URL || 'sua_string_local_aqui', {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    logging: false
+});
 
 module.exports = sequelize;
