8aeb8c27de331f78f9c44a0f969e40939a982731
Joao Vitor <jvitorgotardo0514@gmail.com>
2026-05-03 17:42:06 -0300

    Conexão do front-end com o Render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:3002'
+const api = axios.create({ 
+  baseURL: 'https://ranking-tenis-api.onrender.com' 
 });
 
 export default api;
